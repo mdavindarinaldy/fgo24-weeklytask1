7011b3e6b1a97152dec8a61fdbe6ab0dca8a7312
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-21 10:27:52 +0700

    fixes the redundant codes

--- wt1-javascript2.js
+++ wt1-javascript2.js
@@ -11,17 +11,15 @@ rl.question("Berapa total belanja Anda : ", function calculate(total) {
     let diskon = 0
     total = Number(total)
     // console.log(typeof total) // cek tipe data variabel total
-    if (total >= 1000000) {
-        diskon = 0.1
-    } else if (total >= 500000 && total <1000000){
-        diskon = 0.05
-    } else {
-        diskon = 0
-    }
+    if (total >= 1000000) { diskon = 0.1 } 
+    if (total >= 500000 && total <1000000) { diskon = 0.05 }
+    // } else {         // redundant
+    //     diskon = 0
+    // }
     
     let price = total - (total * diskon)
     
-    if (diskon > 0) {
+    if (diskon) { // truthy, kalau menggunakan perbandingan diskon > 0 jadi redundant
         console.log(`Anda mendapatkan diskon sebesar ${diskon*100}%, total belanja anda menjadi Rp ${price}`)
     } else {
         console.log(`Anda tidak mendapatkan diskon, total belanja anda Rp ${price}`)
